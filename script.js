6a2b52c87858a2bff429d7a2a7bb8664370f81c7
Andrew Kaye <andykayenc@yahoo.com>
2026-05-07 15:24:22 -0700

    Renamed functions for added clarity

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-async function fetchData(){
+/*async function fetchData(){
     const response = await fetch("https://pgvh253inp3c4wkphsv2uwrequ0zzjwe.lambda-url.us-east-2.on.aws/");
     const data = await response.json();
     console.log(data);
@@ -8,22 +8,17 @@ console.log(data);
 //send data to database once it's created
 
 //return data;
-}
+}*/
 
 async function handleButtonClick() {
     //runScript();
-    fetchData();
-    //window.location.href = 'downloadRecipe.html';
+
+    //fetchData(result);
+
+    window.location.href = 'downloadRecipe.html';
 }
 
-async function writeRecipeJSON(URL, name, ingredients, instructions) {
-    data = {
-        "url": URL,
-        "name": name,
-        "ingredients": ingredients,
-        "instructions": instructions
-    }
-
+async function sendData(URL, name, ingredients, instructions) {
     const response = await fetch("https://pgvh253inp3c4wkphsv2uwrequ0zzjwe.lambda-url.us-east-2.on.aws/",
      {
         method: "POST",
@@ -32,14 +27,16 @@ async function writeRecipeJSON(URL, name, ingredients, instructions) {
         },
         body: JSON.stringify({
             url: URL,
-            name,
-            ingredients,
-            instructions
+            name: name,
+            ingredients: ingredients,
+            instructions: instructions
         })
     });
 
     if(!response.ok){
-        throw new Error("Failed to send data to the server");   
+       const text = await response.text();
+       console.error("Lambda response:", text);
+       throw new Error(`HTTP error ${response.status}: ${text}`);
     }
 
     const result = await response.json();
