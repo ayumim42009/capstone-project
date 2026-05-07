7336d276dcbe40749b2e89327f39da73eeaee6cd
Traian Manolescu <111922593+Manoltra@users.noreply.github.com>
2026-05-07 11:14:20 -0700

    Adapted to JavaScript scraping
    Added package.json and scraper.js
    GOALS: move scraper.js to script.js and ensure the button is able to get a link to the allrecipe webpage.

---
+++ scraper.js
@@ -0,0 +1,72 @@
+import * as cheerio from 'cheerio';
+import fs from 'fs';
+import cloudscraper from 'cloudscraper';
+
+const URL = "https://www.allrecipes.com/recipe/279991/lemony-almond-ricotta-cookies/";
+
+const numberWords = [
+    "One", "Two", "Three", "Four",
+    "Five", "Six", "Seven", "Eight", "Nine",
+    "Ten", "Eleven", "Twelve", "Thirteen",
+    "Fourteen", "Fifteen", "Sixteen",
+    "Seventeen", "Eighteen", "Nineteen",
+    "Twenty"
+];
+
+async function scrape() {
+
+    const html = await cloudscraper.get(URL);
+
+    const $ = cheerio.load(html);
+
+    // NAME
+    const name = $("h1.article-heading.text-headline-400")
+        .text()
+        .trim();
+
+    // INGREDIENTS
+    const ingredients = [];
+
+    $("ul.mm-recipes-structured-ingredients__list li")
+        .each((i, el) => {
+
+            ingredients.push(
+                $(el)
+                    .text()
+                    .trim()
+                    .replace(/\s+/g, " ")
+            );
+        });
+
+    // INSTRUCTIONS
+    const instructions = {};
+
+    $("ol.comp.mntl-sc-block.mntl-sc-block-startgroup.mntl-sc-block-group--OL li")
+        .each((i, el) => {
+
+            const stepName = `Step ${numberWords[i + 1] || (i + 1)}`;
+
+            instructions[stepName] = $(el)
+                .text()
+                .trim()
+                .replace(/\s+/g, " ");
+        });
+
+    const recipe = {
+        name,
+        ingredients,
+        instructions
+    };
+
+    // WRITE JSON FILE
+    fs.writeFileSync(
+        "recipe.json",
+        JSON.stringify(recipe, null, 4)
+    );
+
+    return recipe;
+}
+
+const recipe = await scrape();
+
+console.log(recipe);
