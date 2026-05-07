06cbf76d917a9b05db60490797b03693d30cbe3e
Andrew Kaye <andykayenc@yahoo.com>
2026-05-06 21:49:29 -0700

    Added function to capture website url a,d write it to a JSON file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function fetchData(){
     try {
-         const response = await fetch("./recipe.json"),
+         const response = await fetch("./recipe.json")
          const data = await response.json();
 // Prints out the JSON file data to ensure that the information is all there
 console.log(data);
@@ -13,12 +13,25 @@ console.log(data);
 }
 
 async function execPythonScript() {
-    const response = await fetch('/resipi.py');
-    const data = await response.json();
+    //pyscript.interpreter.globals.get('click_handler')();
+    const response = await fetch('./resipi.py');
+    const data = await response.text();
     console.log(data.output);
 }
 
-async function handleButtonClick(){
-    await execPythonScript();
+async function handleButtonClick() {
+    //await execPythonScript();
     window.location.href = 'downloadRecipe.html';
+}
+
+function writeRecipeJSON(url, name, ingredients, instructions) {
+    data = {
+        "url": url,
+        "name": name,
+        "ingredients": ingredients,
+        "instructions": instructions
+    }
+
+    const jsonData = JSON.stringify(data);
+    console.log(jsonData);
 }
