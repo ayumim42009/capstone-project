e43f2a273773050c6aeef93e787cb5b6d6431560
Andrew Kaye <andykayenc@yahoo.com>
2026-05-24 16:17:16 -0700

    Removed unused code and added await

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ function downloadJSON(data) {
 
 //other functions to send data to database and redirect to display page
 async function redirectToDisplay() {
-    const recipeName = document.getElementsByClassName("recipe-card-title")[0].textContent;
+    const recipeName = document.getElementsByClassName("recipe-card-title")[0].textContent.trim();
     console.log(recipeName);
     window.location.href = 'displayRecipe.html';
     await readRecipeData(recipeName);
